1152e803b428e8a0a449f5fb5f74da7409ee6bc7
Joe di Stefano <joeydi@okaypl.us>
2024-01-17 17:30:20 -0500

    start on tour card animations

--- tour.js
+++ tour.js
@@ -5,29 +5,162 @@ gsap.registerPlugin(ScrollTrigger);
 
 export default function initTour(element) {
     const video = element.querySelector("video");
+    const pin = element.querySelector(".pin");
+    const time = element.querySelector(".time");
+    const exterior = element.querySelector(".exterior");
+    const access = element.querySelector(".access");
+    const interior1 = element.querySelector(".interior-1");
+    const interior2 = element.querySelector(".interior-2");
+    const mobility = element.querySelector(".mobility");
+    const timeline = gsap.timeline({
+        scrollTrigger: {
+            trigger: element,
+            start: "top bottom",
+            end: "bottom 50%",
+            scrub: true,
+        },
+    });
 
-    gsap.fromTo(
+    timeline.fromTo(
         video,
         {
             currentTime: 0,
         },
         {
             currentTime: 30.65,
+            duration: 30.65,
             ease: "none",
-            scrollTrigger: {
-                // markers: true,
-                trigger: element,
-                start: "top bottom",
-                end: "bottom top",
-                scrub: true,
+            onUpdate: () => {
+                time.innerText = video.currentTime.toFixed(1);
             },
-        }
+        },
+        0
+    );
+
+    timeline.to(
+        exterior,
+        {
+            opacity: 1,
+            yPercent: -50,
+            duration: 1,
+            ease: "none",
+        },
+        2
+    );
+
+    timeline.to(
+        exterior,
+        {
+            opacity: 0,
+            yPercent: -150,
+            duration: 1,
+            ease: "none",
+        },
+        6.5
+    );
+
+    timeline.to(
+        access,
+        {
+            opacity: 1,
+            yPercent: -50,
+            duration: 1,
+            ease: "none",
+        },
+        7.5
+    );
+
+    timeline.to(
+        access,
+        {
+            opacity: 0,
+            yPercent: -150,
+            duration: 1,
+            ease: "none",
+        },
+        10
+    );
+
+    timeline.to(
+        interior1,
+        {
+            opacity: 1,
+            yPercent: -50,
+            duration: 1,
+            ease: "none",
+        },
+        11.5
+    );
+
+    timeline.to(
+        interior1,
+        {
+            opacity: 0,
+            yPercent: -150,
+            duration: 1,
+            ease: "none",
+        },
+        14
+    );
+
+    timeline.to(
+        interior2,
+        {
+            opacity: 1,
+            yPercent: -50,
+            duration: 1,
+            ease: "none",
+        },
+        15.5
+    );
+
+    timeline.to(
+        interior2,
+        {
+            opacity: 0,
+            yPercent: -150,
+            duration: 1,
+            ease: "none",
+        },
+        19
+    );
+
+    timeline.to(
+        mobility,
+        {
+            opacity: 1,
+            yPercent: -50,
+            duration: 1,
+            ease: "none",
+        },
+        20.5
+    );
+
+    timeline.to(
+        mobility,
+        {
+            yPercent: -250,
+            duration: 1,
+            ease: "none",
+        },
+        22.5
+    );
+
+    timeline.to(
+        mobility,
+        {
+            opacity: 0,
+            yPercent: -500,
+            duration: 1,
+            ease: "none",
+        },
+        27
     );
 
     ScrollTrigger.create({
         trigger: element,
-        pin: video,
+        pin: pin,
         start: () => `top ${window.innerWidth * 0.0638}`,
-        end: "bottom 50%",
+        end: "bottom top",
     });
 }
