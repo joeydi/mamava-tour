e3e4c9414bf0e736c7474fd074000cf46b2e1ef2
Joe di Stefano <joeydi@okaypl.us>
2024-01-23 14:36:04 -0500

    setup tour nav

--- tour.js
+++ tour.js
@@ -1,15 +1,19 @@
 import gsap from "gsap";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import { SplitText } from "gsap/SplitText";
 
-gsap.registerPlugin(ScrollTrigger, SplitText);
+gsap.registerPlugin(ScrollToPlugin, ScrollTrigger, SplitText);
 
 export default function initTour(element) {
     if (!element) return;
 
     const video = element.querySelector("video");
     const pin = element.querySelector(".pin");
-    const time = element.querySelector(".time");
+    const timeLabel = element.querySelector(".time");
+    const navPrevious = element.querySelector(".nav .previous");
+    const navNext = element.querySelector(".nav .next");
+    const navLabel = element.querySelector(".nav .label");
     const exterior = element.querySelector(".exterior");
     const exteriorSplit = new SplitText(exterior.querySelector("h2"), { type: "lines" });
     const access = element.querySelector(".access");
@@ -30,13 +34,35 @@ export default function initTour(element) {
         opacity: 0,
     });
 
+    const getSectionLabel = (time) => {
+        if (time < 7.5) {
+            return "Exterior";
+        }
+
+        if (time < 11) {
+            return "Access";
+        }
+
+        if (time < 20) {
+            return "Interior";
+        }
+
+        return "Mobility";
+    };
+
     const timeline = gsap.timeline({
         scrollTrigger: {
-            // markers: true,
             trigger: element,
             start: "top bottom",
             end: "bottom 75%",
             scrub: true,
+            onUpdate: () => {
+                const time = timeline.progress() * timeline.duration();
+                timeLabel.innerText = time.toFixed(1);
+
+                const label = getSectionLabel(time);
+                navLabel.innerText = label;
+            },
         },
     });
 
@@ -49,9 +75,6 @@ export default function initTour(element) {
             currentTime: 29.7,
             duration: 29.7,
             ease: "none",
-            onUpdate: () => {
-                time.innerText = video.currentTime.toFixed(1);
-            },
         },
         0
     );
@@ -90,6 +113,8 @@ export default function initTour(element) {
         6.5
     );
 
+    timeline.addLabel("Exterior", 6.5);
+
     timeline.to(
         access,
         {
@@ -123,6 +148,8 @@ export default function initTour(element) {
         },
         10
     );
+
+    timeline.addLabel("Access", 10);
 
     timeline.to(
         interior1,
@@ -158,6 +185,8 @@ export default function initTour(element) {
         14.5
     );
 
+    timeline.addLabel("Interior 1", 14.2);
+
     timeline.to(
         interior2,
         {
@@ -192,6 +221,8 @@ export default function initTour(element) {
         19
     );
 
+    timeline.addLabel("Interior 2", 18.7);
+
     timeline.to(
         mobility,
         {
@@ -202,6 +233,8 @@ export default function initTour(element) {
         },
         20
     );
+
+    timeline.addLabel("Mobility", 26.8);
 
     timeline.to(
         mobilitySplit.lines,
@@ -236,6 +269,7 @@ export default function initTour(element) {
         27
     );
 
+    // Pin the tour
     ScrollTrigger.create({
         trigger: element,
         pin: pin,
@@ -243,6 +277,7 @@ export default function initTour(element) {
         end: "bottom top",
     });
 
+    // Scroll the video out at half speed
     gsap.to(video, {
         yPercent: -50,
         scrollTrigger: {
@@ -251,5 +286,38 @@ export default function initTour(element) {
             end: "bottom top",
             scrub: true,
         },
+    });
+
+    const playToLabel = (label) => {
+        const scrollPosition = timeline.scrollTrigger.labelToScroll(label);
+        const scrollDelta = Math.abs(document.documentElement.scrollTop - scrollPosition);
+        const scrollDuration = (scrollDelta / window.innerHeight) * 2;
+
+        gsap.to(window, {
+            scrollTo: {
+                y: scrollPosition,
+                autoKill: true,
+            },
+            duration: scrollDuration,
+            ease: "none",
+        });
+    };
+
+    navPrevious.addEventListener("click", () => {
+        const currentTime = timeline.progress() * timeline.duration();
+        const previousTimelineLabel = timeline.previousLabel(currentTime - 0.01);
+
+        if (previousTimelineLabel) {
+            playToLabel(previousTimelineLabel);
+        }
+    });
+
+    navNext.addEventListener("click", () => {
+        const currentTime = timeline.progress() * timeline.duration();
+        const nextTimelineLabel = timeline.nextLabel(currentTime + 0.01);
+
+        if (nextTimelineLabel) {
+            playToLabel(nextTimelineLabel);
+        }
     });
 }
