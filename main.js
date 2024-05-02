e3e51c966d575991f1742ae956f9cc07d1d8e085
Joe di Stefano <joeydi@okaypl.us>
2024-05-02 15:56:37 -0400

    wrap all function calls in DOMContentLoaded

--- main.js
+++ main.js
@@ -4,8 +4,10 @@ import initFeatures from "./features.js";
 import initSpecs from "./specs.js";
 import "./styles/main.scss";
 
-initHero(document.querySelector("#hero"));
-initTour(document.querySelector("#tour"));
-initFeatures(document.querySelector("#features-section-1"));
-initFeatures(document.querySelector("#features-section-2"));
-initSpecs(document.querySelector("#specs"));
+document.addEventListener("DOMContentLoaded", () => {
+    initHero(document.querySelector("#hero"));
+    initTour(document.querySelector("#tour"));
+    initFeatures(document.querySelector("#features-section-1"));
+    initFeatures(document.querySelector("#features-section-2"));
+    initSpecs(document.querySelector("#specs"));
+});
